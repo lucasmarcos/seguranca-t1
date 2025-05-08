75f25b17f69306412cac5d7fe5e2ca1b783d6dc1
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-05-08 13:41:12 -0300

    package

--- src/alice.ts
+++ src/alice.ts
@@ -1,4 +1,7 @@
 import { createServer } from "node:net";
+import { createHash } from "node:crypto";
+
+const hash = createHash("sha256");
 
 const server = createServer((client) => {
   console.log(client);
--- src/bob.ts
+++ src/bob.ts
@@ -1,4 +1,7 @@
 import { createConnection } from "node:net";
+import { createHash } from "node:crypto";
+
+const hash = createHash("sha256");
 
 const client = createConnection({ path: "./socket" }, () => {
   console.log(client);
