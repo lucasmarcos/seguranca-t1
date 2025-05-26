c1c4d9e4a9fa16d2e0b9a67b5525da0993569578
Lucas da Silva Marcos <640383+lucasmarcos@users.noreply.github.com>
2025-05-26 17:26:09 -0300

    chaves simetricas

--- src/alice.ts
+++ src/alice.ts
@@ -1,4 +1,7 @@
 import { createServer } from "node:net";
+import { simetrica } from "./lib.js";
+
+const chaveSimetrica = simetrica();
 
 const server = createServer((client) => {
   console.log(client);
--- src/bob.ts
+++ src/bob.ts
@@ -1,4 +1,7 @@
 import { createConnection } from "node:net";
+import { simetrica } from "./lib.js";
+
+const chaveSimetrica = simetrica();
 
 const client = createConnection({ path: "./socket" }, () => {
   console.log(client);
